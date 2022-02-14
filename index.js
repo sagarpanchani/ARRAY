50685b00162b1a556aaa0e219ff6713600e7e5bb
sagarpanchani <sagarkmar92@gmail.com>
2022-02-14 17:46:42 +0500

    ARRAY METHOD

---
+++ index.js
@@ -0,0 +1,25 @@
+
+// ARRAY
+
+var colors = ["red", "yellow","Blue", "Green" ]
+console.log(colors)
+
+// UPDATE
+
+var colors = ["red", "yellow","Blue", "Green" ]
+colors[0] = "Red and purple"
+console.log(colors)
+
+// POP
+
+var colors = ["red", "yellow","Blue", "Green" ]
+
+colors.pop()
+console.log(colors)
+
+// PUSH
+
+var colors = ["red", "yellow","Blue", "Green" ]
+
+colors.push("white")
+console.log(colors)
